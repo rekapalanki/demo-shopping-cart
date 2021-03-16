43926f2ca9c193a1b87141b8b6b088c1628354ed
Reka Palanki <reka.palanki@gmail.com>
2021-03-16 16:06:57 +0100

    updating item values. Sum up total value

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ cards.insertAdjacentElement('beforebegin', heading1);
 
 const buyButtons = document.querySelectorAll('.buy');
 const endOfCart = document.querySelector('.endOfCart');
+const cartSum = document.getElementById('cartSum');
+const cartValueWrapper = document.querySelector('.cartValueWrapper');
+const cartValue = document.getElementById('cartValue');
 
 // If buttons are clicked, add items to the cart.
 // 01 Add click event listener 
@@ -23,19 +26,43 @@ const eventHandler = (event) => {
     event.currentTarget.nextElementSibling.textContent = counter;
     // add item to the cartItem
     const cartItemHtml = `
-    <div class="cartItem">
+    <div class="cartItem" id=${event.currentTarget.previousElementSibling.previousElementSibling.textContent}>
         <p>${event.currentTarget.previousElementSibling.previousElementSibling.textContent}</p>
         <p>${parseFloat(event.currentTarget.nextElementSibling.textContent)}</p>
-        <p>${parseFloat(event.currentTarget.nextElementSibling.nextElementSibling.textContent)}</p>
-        <p>${parseFloat(event.currentTarget.nextElementSibling.textContent)*parseFloat(event.currentTarget.nextElementSibling.nextElementSibling.textContent)}</p>
+        <p>${parseFloat(event.currentTarget.nextElementSibling.nextElementSibling.textContent)} AUD</p>
+        <p>${parseFloat(event.currentTarget.nextElementSibling.textContent)*parseFloat(event.currentTarget.nextElementSibling.nextElementSibling.textContent)} AUD</p>
     </div>`;
     const cartItemRange = document.createRange().createContextualFragment(cartItemHtml);
     const cartItem = cartItemRange.querySelector('div');
-    endOfCart.insertAdjacentElement('beforebegin', cartItem);
+    // if there is no itemId div, create one. Else, update existing one.
+    const itemDiv = document.getElementById(cartItem.id);
+    // check if item has been created
+    // console.log(itemIdDiv === null);
+    if (itemDiv === null) {
+        console.log('true');
+        endOfCart.insertAdjacentElement('beforebegin', cartItem);
+    } else {
+        console.log('false');
+        // update existing div
+        itemDiv.children[1].textContent = parseFloat(cartItem.children[1].textContent);
+        itemDiv.children[2].textContent = `${parseFloat(cartItem.children[2].textContent)} AUD`
+        itemDiv.children[3].textContent = `${parseFloat(cartItem.children[3].textContent)} AUD`;
+    }
+    // Sum up total value
+    // 01 Store original value in a new variable
+    let cartSumText = parseFloat(cartSum.textContent);
+    // 02 Get value to be added up
+    cartSumText += parseFloat(cartItem.children[2].textContent);
+    // update cartsum.textContent
+    cartSum.textContent = `${cartSumText} AUD`;
+    cartValue.textContent = `${cartSumText} AUD`;
+    cartValueWrapper.classList.remove('displayNone');
 }
 
 const clickEventListener = (button) => {
-    button.addEventListener('click', eventHandler)
+    button.addEventListener('click', eventHandler);
 }
 
-buyButtons.forEach(clickEventListener)
+buyButtons.forEach(clickEventListener);
+
+// Add remove button to cart items
